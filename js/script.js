a809a0f4dd5f983b6b20e8cd3b96cd84728081ee
InsanityJs <maks.naymenko@gmail.com>
2020-11-05 11:37:17 +0200

    Maked responcive slider

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-////$(document).ready(function () {
-////  $(".carousel__iner").slick({
-////    speed: 1200,
-////    adaptiveHeight: false,
-////    prevArrow:
-////      '<button type="button" class="slick-prev"><img src="icons/back.svg"></img></button>',
-////    nextArrow:
-////      '<button type="button" class="slick-next"><img src="icons/next.svg"></img></button>',
-////  });
-////});
-////$(document).ready(function () {
-////  $(".owl-carousel").owlCarousel({
-////    loop: true,
-////    items: 1,
-////    nav: true,
-////  });
-////});
-//const owl = $(".owl-carousel");
-//owl.owlCarousel({
-//  loop: true,
-//  items: 1,
-//  dots: false,
-//});
-//$(".next").click(function () {
-//  owl.trigger("next.owl.carousel", [1200]);
-//});
-//$(".prev").click(function () {
-//  owl.trigger("prev.owl.carousel", [1200]);
-//});
 const slider = tns({
   container: ".slider",
   slideBy: "page",
@@ -38,10 +9,20 @@ const slider = tns({
   preventActionWhenRunning: true,
   controls: false,
   responsive: {
-    768: {
+	 998: {
       slideBy: "page",
       autoplay: true,
       nav: false,
+      speed: 1200,
+      autoplayButtonOutput: false,
+      mouseDrag: true,
+      preventActionWhenRunning: true,
+      controls: false,
+    },
+    768: {
+      slideBy: "page",
+      autoplay: true,
+      nav: true,
       speed: 1200,
       autoplayButtonOutput: false,
       mouseDrag: true,
