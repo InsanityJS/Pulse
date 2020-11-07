a59a430e40e7f8912528c4cc3871019142004a92
Naumenko <maks.naymenko@gmail.com>
2020-11-07 18:10:35 +0200

    Maked Catalog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Скрипт Слайдера
 const slider = tns({
   container: ".slider",
   slideBy: "page",
@@ -9,7 +10,7 @@ const slider = tns({
   preventActionWhenRunning: true,
   controls: false,
   responsive: {
-	 998: {
+    998: {
       slideBy: "page",
       autoplay: true,
       nav: false,
@@ -37,9 +38,44 @@ const slider = tns({
     },
   },
 });
+// Срипт для стрелок назад и вперед
 document.querySelector(".next").onclick = function () {
   slider.goTo("next");
 };
 document.querySelector(".prev").onclick = function () {
   slider.goTo("prev");
 };
+// Скрипт для табов
+(function ($) {
+  $(function () {
+    $("ul.catalog__tabs").on(
+      "click",
+      "li:not(.catalog__tab_active)",
+      function () {
+        $(this)
+          .addClass("catalog__tab_active")
+          .siblings()
+          .removeClass("catalog__tab_active")
+          .closest("div.container")
+          .find("div.catalog__content")
+          .removeClass("catalog__content_active")
+          .eq($(this).index())
+          .addClass("catalog__content_active");
+      }
+    );
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+})(jQuery);
